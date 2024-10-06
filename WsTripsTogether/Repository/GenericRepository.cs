d6f75b53777e39bc602e04e7f919a02843226fde
lorenzoalessi <alessilorenzo02@gmail.com>
2024-10-06 19:48:34 +0200

    Added generic repository

---
+++ WsTripsTogether/Repository/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WsTripsTogether.Model;
+
+namespace WsTripsTogether.Repository;
+
+public class GenericRepository<TDbEntity>(Context context) : IGenericRepository<TDbEntity> where TDbEntity : class
+{
+    protected readonly Context Context = context;
+
+    public async Task<List<TDbEntity>> GetAsync() => await Context.Set<TDbEntity>().ToListAsync();
+
+    public async Task<TDbEntity?> GetByIdAsync(int id) => await Context.Set<TDbEntity>().FindAsync(id);
+
+    public async Task<TDbEntity> AddAsync(TDbEntity dbEntity)
+    {
+        await Context.Set<TDbEntity>().AddAsync(dbEntity);
+        await Context.SaveChangesAsync();
+
+        return dbEntity;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity == null)
+            return false;
+
+        Context.Set<TDbEntity>().Remove(entity);
+        await Context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task SaveAsync() => await Context.SaveChangesAsync();
+}
---
+++ WsTripsTogether/Repository/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace WsTripsTogether.Repository;
+
+public interface IGenericRepository<TDbEntity>
+{
+    Task<List<TDbEntity>> GetAsync();
+    Task<TDbEntity?> GetByIdAsync(int id);
+    Task<TDbEntity> AddAsync(TDbEntity dbEntity);
+    Task<bool> DeleteAsync(int id);
+    Task SaveAsync();
+}
